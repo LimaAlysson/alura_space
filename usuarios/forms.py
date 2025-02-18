34f3bd8cab7b380f0cbee7ec32605755569be4a5
LimaAlysson <alyssonredes32@gmail.com>
2025-02-18 16:28:17 -0300

    feature/personalizando o form através do forms.py

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -4,13 +4,14 @@ class LoginForms(forms.Form):
     username = forms.CharField(
         label='Usuario', 
         required=True, 
-        max_length=100
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nome de usuario'})
         )
     
     password = forms.CharField(
         label='Senha', 
         required=True, 
         max_length=70, 
-        widget=forms.PasswordInput()
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Senha'})
         )
     
