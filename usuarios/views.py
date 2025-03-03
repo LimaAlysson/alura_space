7fb586c96707582692d779c0e3f4542002ae89b5
LimaAlysson <alyssonredes32@gmail.com>
2025-03-03 15:59:42 -0300

    adicionando messages de sucesso e erro nos templates

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from usuarios.forms import LoginForms, CadastroForms
 from django.contrib.auth.models import User
 from django.contrib import auth
+from django.contrib import messages
 
 def login(request):
     form = LoginForms()
@@ -20,8 +21,10 @@ def login(request):
         )
         if user is not None:
             auth.login(request, user)
+            messages.success(request, f"{name} logado com sucesso")
             return redirect('index')
         else:
+            messages.error(request, "Usuário ou senha incorretos")
             return redirect('login')
 
     return render(request, 'usuarios/login.html', {"form": form})
@@ -34,6 +37,7 @@ def cadastro(request):
         
         if form.is_valid():
             if form["password_1"].value() != form["password_2"].value():
+                messages.error(request, "Senhas não coincidem!")
                 return redirect('cadastro')
 
             name = form["signup_name"].value()
@@ -41,6 +45,7 @@ def cadastro(request):
             password = form["password_1"].value()
 
             if User.objects.filter(username=name).exists():
+                messages.error(request, "Usuário já existe!")
                 return redirect('cadastro')
             
             user = User.objects.create_user(
@@ -49,6 +54,7 @@ def cadastro(request):
                 password=password,
             )
             user.save()
+            messages.sucess(request, "Cadastro realizado com sucesso!")
             return redirect('login')
 
     return render(request, 'usuarios/cadastro.html', {"form": form})
