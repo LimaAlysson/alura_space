634c4b25fbacbc956fa0518bd254bf3b24841406
LimaAlysson <alyssonredes32@gmail.com>
2025-03-05 14:24:19 -0300

    feature/criando formulario para inserir imagens

---
+++ apps/galeria/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+
+from apps.galeria.models import Fotografia
+
+#criando form através de um model existente
+class FotografiaForms(forms.ModelForm):
+    class Meta:
+        model = Fotografia
+        exclude = ['publicada'] #campos que não é necessario no form
+
+        widgets = {
+            'nome': forms.TextInput(attrs={'class':'form-control'}),
+            'legenda': forms.TextInput(attrs={'class':'form-control'}),
+            'categoria': forms.Select(attrs={'class':'form-control'}),
+            'descricao': forms.Textarea(attrs={'class':'form-control'}),
+            'foto': forms.FileInput(attrs={'class':'form-control'}),
+            'data_fotografia': forms.DateInput(
+                format = '%d/%m/%Y',
+                attrs={
+                    'type':'date',
+                    'class':'form-control'
+                    }
+            ),
+            'usuario': forms.Select(attrs={'class':'form-control'}),
+        }
