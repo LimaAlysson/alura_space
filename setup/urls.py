d707802e8f61bb8e4bb94665b5f2d87793fb78ed
LimaAlysson <alyssonredes32@gmail.com>
2025-03-05 18:20:50 -0300

    feature/inserindo arquivos estáticos no S3 da AWS

--- setup/urls.py
+++ setup/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.galeria.urls')),
     path('',include('apps.usuarios.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # utilizando as configurações de static do arquivo settings
+]
+
+
+if settings.DEBUG:
+    # utilizando as configurações de static do arquivo settings
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
